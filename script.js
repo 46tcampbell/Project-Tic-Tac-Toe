fc5b5ef6bd7abd279b61b2af356d80a4c04e7009
46tcampbell <157092336+46tcampbell@users.noreply.github.com>
2025-10-30 11:41:58 -0500

    Add logic to create initial 3x3 gameboard
    Decided to utilize the nested loop to create a 2d array like the example
    site used as I thought that was a simple and easy way to do it.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+function createGameboard() {
+  const rows = 3;
+  const columns = 3;
+  const gameboard = [];
+
+  for (let i = 0; i < rows; i++) {
+    gameboard[i] = [];
+    for (let j = 0; j < columns; j++) {
+      gameboard[i].push([]);
+    }
+  }
+  return { gameboard };
+}
