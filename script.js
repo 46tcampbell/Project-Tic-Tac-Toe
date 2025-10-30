a894c40d5f492824653b783f620d3c7c6357b43d
46tcampbell <157092336+46tcampbell@users.noreply.github.com>
2025-10-30 13:29:29 -0500

    Add logic for the gameplay flow
    I relied heavily on the example article for making the gameplay flow
    work for at least marking the gameboard by entering a row and column.
    This felt wrong, but a quick look up on Discord made me feel like it was
    okay to borrow these ideas as the alternative is to come up with a brand
    new way of doing this.
    
    Next step is to add win logic.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function createGameBoard() {
   for (let i = 0; i < rows; i++) {
     gameBoard[i] = [];
     for (let j = 0; j < columns; j++) {
-      gameBoard[i].push([]);
+      gameBoard[i].push(undefined);
     }
   }
 
@@ -17,6 +17,62 @@ function createGameBoard() {
   return { gameBoard, addMarker };
 }
 
-function createPlayer(name, marker) {
-  return { name, marker };
+// function createPlayer(name, marker) {
+//   return { name, marker };
+// }
+
+function createGameplay(
+  playerOneName = 'Player One',
+  playerTwoName = 'Player Two'
+) {
+  const gameBoard = createGameBoard();
+  const players = [
+    {
+      name: playerOneName,
+      marker: 'X',
+    },
+    {
+      name: playerTwoName,
+      marker: 'O',
+    },
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    console.log(gameBoard.gameBoard);
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const playRound = (row, column) => {
+    console.log(
+      `Marking square at row(${row}) and 
+      column(${column}) with ${getActivePlayer().name}'s Marker(${
+        getActivePlayer().marker
+      })`
+    );
+    gameBoard.addMarker(row, column, getActivePlayer().marker);
+
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  printNewRound();
+
+  return { playRound };
+  // gameOver: function (winningPlayer) {
+  //   console.log('Congratulations!');
+  //   console.log(winningPlayer.name + ' is the winner!');
+  // },
+  // printName: function (player) {
+  //   console.log(player.name);
+  // },
 }
+
+const game = createGameplay();
