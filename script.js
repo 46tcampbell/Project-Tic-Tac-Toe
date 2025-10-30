f85fdc0efb8f97d3f5780788ca8ee7fada3a086c
46tcampbell <157092336+46tcampbell@users.noreply.github.com>
2025-10-30 14:53:28 -0500

    Add game draw logic
    Decided to use another if statement which was shorter this time, but not
    sure if there is a better way to write the logic.

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
+//Below function creates the actual gameboard to play on
+
 function createGameBoard() {
   const rows = 3;
   const columns = 3;
   const gameBoard = [];
 
+  //below nested for loop creates a 2-dimensional array
+  //to create a 3 x 3 grid
   for (let i = 0; i < rows; i++) {
     gameBoard[i] = [];
     for (let j = 0; j < columns; j++) {
-      gameBoard[i].push('0');
+      gameBoard[i].push(undefined);
     }
   }
-
+  //Below function adds an 'X' or 'O' to gameBoard
   const addMarker = (row, column, marker) => {
     gameBoard[row][column] = marker;
   };
-
+  //Below returns an object when function is executed that gives access to
+  //gameBoard and addMarker function
   return { gameBoard, addMarker };
 }
 
-// function createPlayer(name, marker) {
-//   return { name, marker };
-// }
-
+//Below function controls the gameplay
 function createGameplay(
   playerOneName = 'Player One',
   playerTwoName = 'Player Two'
@@ -50,10 +52,15 @@ function createGameplay(
     console.log(`${getActivePlayer().name}'s turn.`);
   };
 
-  const gameOver = (winningPlayer) => {
+  const gameOverWinner = (winningPlayer) => {
     console.log(gameBoard.gameBoard);
     console.log('Congratulations!');
     console.log(`${winningPlayer.name} is the winner!`);
+  };
+
+  const gameOverDraw = (winningPlayer) => {
+    console.log(gameBoard.gameBoard);
+    console.log('Draw!');
   };
 
   const playRound = (row, column) => {
@@ -64,7 +71,7 @@ function createGameplay(
       })`
     );
     gameBoard.addMarker(row, column, getActivePlayer().marker);
-
+    //Below if statement checks if there is a winner after the current player moves.
     if (
       (gameBoard.gameBoard[0][0] === `${getActivePlayer().marker}` &&
         gameBoard.gameBoard[0][1] === `${getActivePlayer().marker}` &&
@@ -85,7 +92,15 @@ function createGameplay(
         gameBoard.gameBoard[1][1] === `${getActivePlayer().marker}` &&
         gameBoard.gameBoard[2][2] === `${getActivePlayer().marker}`)
     ) {
-      return gameOver(getActivePlayer());
+      return gameOverWinner(getActivePlayer());
+      //Below else if statement checks if there are any remaining undefined squares,
+      //if not, a draw is called.
+    } else if (
+      gameBoard.gameBoard[0].includes(undefined) === false &&
+      gameBoard.gameBoard[1].includes(undefined) === false &&
+      gameBoard.gameBoard[2].includes(undefined) === false
+    ) {
+      return gameOverDraw();
     }
     switchPlayerTurn();
     printNewRound();
@@ -94,9 +109,6 @@ function createGameplay(
   printNewRound();
 
   return { playRound, getActivePlayer };
-  // printName: function (player) {
-  //   console.log(player.name);
-  // },
 }
 
 const game = createGameplay();
